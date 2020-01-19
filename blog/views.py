2f563bb9ea7303c7abb06a763265fe4459246c57
biggreenrm <biggreen.rm@gmail.comgit config --global user.email biggreen.rm@gmail.com>
2020-01-19 15:07:54 +0000

    Try to merge server

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+"""Первое представление, которое принимает запрос в качестве аргумента
+и рендерит страницу с постами (создаёт шаблон)"""
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
