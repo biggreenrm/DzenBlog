03c75c6b1507b171979d87c24b115dbd723a7a46
biggreenrm <biggreen.rm@gmail.com>
2020-02-21 08:53:01 +0300

    [Modify] blog.urls by adding path to share.html

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'), #'' - этот шаблон соответствует пустой строке
     path('post/<slug>/', views.post_detail, name='post_detail'),
+    path('post/<slug>/share', views.post_share, name='post_share'),
     path('new/', views.post_new, name='post_new'),
     path('post/<slug>/edit/', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
@@ -16,5 +17,4 @@ urlpatterns = [
     path('post/<slug>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
     path('comment/<slug>/approve/', views.comment_approve, name='comment_approve'),
     path('comment/<slug>/remove/', views.comment_remove, name='comment_remove'),
-      
 ]
