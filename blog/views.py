9509e4dce990c7c7e1789c7b76f8d6ece1d773b4
SergeyGurkov <biggreen.rm@gmail.com>
2020-07-15 19:15:23 +0300

    [DELETE] repeated import

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.db.models import Count
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 from django.utils import timezone
