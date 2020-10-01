90d8051639e3bf40e3164b6185cb73663c21c03b
biggreenrm <59133420+biggreenrm@users.noreply.github.com>
2020-10-01 14:49:20 +0300

    Delete private_settings.py

--- mysite/private_settings.py
+++
@@ -1,9 +0,0 @@
-#SMTP-settings for sharing articles by email
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'biggreen.rm@gmail.com'
-EMAIL_HOST_PASSWORD = 'whoisyourdaddy'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p9su76jy*(rb%dstki=^!5+cwhc#7fhih-59y9fc&0!32d+5=r'
